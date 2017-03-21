525487a898cac80dfe64aa790857e96977f695ef
jeanbdx <jean.arhancetebehere@gmail.com>
2017-03-21 18:53:04 +0100

    Modification des chemins & connection a la base revue

---
+++ Includes/header.php
@@ -0,0 +1,76 @@
+<?php require_once ("Includes/session.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="bower_components/materialize/dist/css/materialize.min.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      
+    </head>
+    <body>
+        <div class="container">
+        <header>
+                        <?php
+                        if (logged_on())
+                        {
+                            echo '<li><a href="/logoff.php">Sign out</a></li>' . "\n";
+                            if (is_admin())
+                            {
+                                /*
+                                echo '<li><a href="/addpage.php">Add</a></li>' . "\n";
+                                echo '<li><a href="/selectpagetoedit.php">Edit</a></li>' . "\n";
+                                echo '<li><a href="/deletepage.php">Delete</a></li>' . "\n";
+                                */                           
+                            }
+                        }
+                        else
+                        {
+                            echo '<li><a href="/logon.php">Login</a></li>' . "\n";
+                            echo '<li><a href="/register.php">Register</a></li>' . "\n";
+                        }
+                        ?>
+                        </ul>
+                    </section>
+                </div>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="/index.php" class="brand-logo">Agence de location</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Créer/Modifier des locataires</a></li>
+        <li><a href="badges.html">Créer des biens</a></li>
+        <li><a href="collapsible.html">Créer un bail</a></li>
+        <li>  <i class="large material-icons">perm_identity</i 
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="sass.html">Créer/Modifier des locataires</a></li>
+        <li><a href="badges.html">Créer des biens</a></li>
+        <li><a href="collapsible.html">Créer un bail</a></li>
+      </ul>
+    </div>
+  </nav>
+
+                            <!-- ON PEUT EFFACER CETTE PARTIE INUTILE-->
+                            <?php
+                                /*
+                                $statement = $databaseConnection->prepare("SELECT id, menulabel FROM pages");
+                                $statement->execute();
+
+                                if($statement->error)
+                                {
+                                    die("Database query failed: " . $statement->error);
+                                }
+
+                                $statement->bind_result($id, $menulabel);
+                                while($statement->fetch())
+                                {
+                                    echo "<li><a href=\"/page.php?pageid=$id\">$menulabel</a></li>\n";
+                                }
+                                */
+                            ?>
+      
+        </header>
+
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
     <?php 
-        require_once ("Includes/simplecms-config.php"); 
+        require_once ("Includes/simplecms-config-sample.php"); 
         require_once  ("Includes/connectDB.php");
         include("Includes/header.php");         
      ?>
 
 
-      <div class="container">
+
         <!-- Page Content goes here -->
  
         <h1> hello world!</h1>
-   </div> <!-- End of outer-wrapper which opens in header.pho -->
+
 
 <?php 
     include ("Includes/footer.php");
