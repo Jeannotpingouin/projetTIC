0053f9ea20be87bfa583d5e4a9bcee66dc8c0769
Jean Arhancetebehere <jarhanc1@emi.u-bordeaux1.fr>
2017-03-21 16:34:26 +0100

    Premier fichier

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+include ("Includes/closeDB.php");        
+?>
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Agence de location</h5>
+                <p class="grey-text text-lighten-4">Ce site est une application pour les agences de location.</p>
+              </div>
+ 
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+          <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="bower_components/materialize/dist/js/materialize.min.js"></script>
+    </body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,16 @@
+    <?php 
+        require_once ("Includes/simplecms-config.php"); 
+        require_once  ("Includes/connectDB.php");
+        include("Includes/header.php");         
+     ?>
+
+
+      <div class="container">
+        <!-- Page Content goes here -->
+ 
+        <h1> hello world!</h1>
+   </div> <!-- End of outer-wrapper which opens in header.pho -->
+
+<?php 
+    include ("Includes/footer.php");
+ ?>
